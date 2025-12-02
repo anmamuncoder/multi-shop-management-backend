6dcd2d6df97bec136c082e9980fb231b9d993779
anmamun0 <anmamun0@gmail.com>
2025-12-02 19:06:27 +0600

    feat(project-setup): initialize Django backend with core packages, caching, Celery and Docker support
    Installed core packages:
     - django , djangorestframework, whitenoise, cors-headers , jazzmin, drf-spectacular , pillow
    
    Database setup with:
     - python-decouple , psycopg2-binary
    
    Added Redis + django-redis for caching.
    
    Configured testing environment:
     - pytest, pytest-django, pytest-cov, factory_boy
    
    Middleware updated:
     - CORS, WhiteNoise
    
    REST framework configured:
     - JWTAuthentication, AutoSchema
    
    Static & media setup:
     - static, staticfiles, media
    
    Integrated Celery with Redis as broker and backend.
    
    Added Docker setup for:
     - web, redis, celery worker, PostgreSQL
    
    Initial architecture ready for scalable backend development.

---
+++ core/settings.py
@@ -0,0 +1,179 @@
+from pathlib import Path
+from decouple import config, Csv
+from datetime import timedelta
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG',default=False,cast=bool)
+
+ALLOWED_HOSTS = []
+
+# Application definition
+CUSTOM_APPS = [
+    'apps.accounts',
+    
+]
+
+INSTALLED_LIBRARY = [
+
+]
+
+INSTALLED_APPS = (
+    [
+        'jazzmin',
+        'corsheaders',
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+    ]   
+    +   INSTALLED_LIBRARY
+    +   CUSTOM_APPS 
+
+)
+
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # Must be at the top
+    'django.middleware.common.CommonMiddleware',
+
+    # Static files (CSS,JS,images) when using Daphne or any ASGI server, because they don’t serve static files by default. 
+    # After adding this middleware, remember to run: `python manage.py collectstatic 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+]
+
+ROOT_URLCONF = "core.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "core.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": config('POSTGRES_DB'),
+        "USER": config('POSTGRES_USER'),
+        "PASSWORD": config('POSTGRES_PASSWORD'),
+        "HOST": config('POSTGRES_HOST',default='localhost'),
+        "PORT": config('POSTGRES_PORT',default='5432')
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+# Static files (CSS, JS, Images)
+STATIC_URL = '/static/' 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # project-level static folder
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # for collectstatic in production
+
+# Media files (user-uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+ 
+REST_FRAMEWORK = {
+   'DEFAULT_PERMISSION_CLASSES': [
+       'rest_framework.permissions.IsAuthenticated',
+   ],
+   'DEFAULT_AUTHENTICATION_CLASSES': [
+       'rest_framework_simplejwt.authentication.JWTAuthentication', 
+   ], 
+    # drf_spectacular for rest_framework
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    
+    'AUTH_HEADER_TYPES': ("Bearer",), 
+    'USER_ID_FIELD': "username",
+    'USER_ID_CLAIM': "username",
+    'ROTATE_REFRESH_TOKENS': True, # Issue a new refresh token every time the old one is used
+    'BLACKLIST_AFTER_ROTATION': True, # Old refresh token is blacklisted immediately after rotation
+}
+
+ 
+# settings.py
+# Run windows Terminal: celery -A core worker --loglevel=INFO -P solo
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/1')
+ 
+
