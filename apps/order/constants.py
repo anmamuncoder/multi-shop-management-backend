689f81d879f143808ab3f91231d412c753258072
anmamun0 <anmamun0@gmail.com>
2025-12-08 15:03:58 +0600

    feat(order): implement Order & OrderItem models with snapshot product data
    - Added Order model: customer user, total_amount, status, created_at, update_at
    - Added OrderItem model: Order , Product, quantity, subtotal_amount, status, created_at, update_at
    - Implemented snapshot fields: product_name, product_price, product_image

---
+++ apps/order/constants.py
@@ -0,0 +1,10 @@
+
+# Order Model
+ORDER_STATUS = [
+    ('pending','Pending'),
+    ('confirmed','Confirmed'),
+    ('processing','Processing'),
+    ('shipped','Shipped'),
+    ('delivered','Delivered'),
+    ('cancelled','Cancelled')
+]
--- apps/order/models.py
+++ apps/order/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+# Internal
+from .constants import ORDER_STATUS
+# External
+from apps.base.models import BaseModel
+from apps.accounts.models import User
+from apps.store.models import Shop,Category,Product,ProductImage,ProductVariant
 
 # Create your models here.
+class Order(BaseModel):
+    customer = models.ForeignKey(User,on_delete=models.PROTECT, limit_choices_to={'role':'customer'} ,related_name='orders')
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    status = models.CharField(choices=ORDER_STATUS, max_length=20, default='pending')
+
+    def __str__(self):
+        return f"Order - {self.customer.email}"
+
+class OrderItem(BaseModel):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE,related_name="items")
+    product = models.ForeignKey(Product,on_delete=models.PROTECT,related_name="orderitems",null=True)
+
+    quantity = models.PositiveIntegerField(default=1)
+    subtotal_amount = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+
+    # Snapshot fields
+    product_name = models.CharField(max_length=200)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    product_image = models.URLField(null=True, blank=True)
+
+    status = models.CharField(choices=ORDER_STATUS, max_length=20, default="pending")
+
+    def __str__(self):
+        return f"Order Item - {self.product_name} - {self.quantity}"
+    
