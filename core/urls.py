3ecdf9d9794cb9d07efc8ecfa8f8a01614e6009a
mehedi23 <satusatusatu0@gmail.com>
2025-12-21 15:34:44 +0600

    docker media and static file

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,6 @@ urlpatterns = (
     + apps_urls
 )
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
